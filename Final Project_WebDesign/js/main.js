418dc890f753ea007592847ca644d68759e4ed4e
GoldMine <connorm1998@gmail.com>
2016-12-05 12:46:09 -0600

    Website

---
+++ Final Project_WebDesign/js/main.js
@@ -0,0 +1,69 @@
+// https://learn.jquery.com/using-jquery-core/document-ready/
+$(document).ready(function(){
+    
+    console.log("hello world!");
+
+});
+
+/*Validate for Registration Form*/
+        $("#email_form").validate({
+            rules: {
+                "email_1": {
+                    required: true,
+                    email: true,
+                },
+                "email_2": {
+                    required: true,
+                    email: true,
+                    equalTo: "#email_1", 
+                },
+                "full_name": {
+                    required: true,
+                },     
+                "phone": {
+                    required: true,
+                    number: true,
+                    minlength: 10,
+                },
+                "address_1": {
+                    required: true,
+                },
+                "address_2": {
+                    required: true,
+                },
+                "city": {
+                    required: true,
+                },
+            },
+            
+            messages: {
+                "email_1": {
+                    required: "Email Address is required",
+                    email: "Please enter a valid email address",
+                },
+                "email_2": {
+                    required: "Email is required",
+                    email: "Please enter a valid email address",
+                    equalTo: "Email addresses must match",
+                },
+                "full_name": {
+                    required: "Full name is required",
+                },
+                "phone": {
+                    required: "Please enter a valid number",
+                    number: "Phone number must be valid",
+                    minLinks: "Please enter a 10 digit phone number",
+                },
+                "address_1": {
+                    required: "Please enter a valid address",
+                },
+                "address_2": {
+                    required: "Please enter a valid address",
+                },
+                "city": {
+                    required: "Please enter a valid city",
+                },
+       },
+    });
+    
+});
